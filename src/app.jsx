7af36fea0087cc169dc0261942c5dd7029b23e4f
rxssufc <r.whitfield1@newcastle.ac.uk>
2025-05-15 17:20:51 +0100

    Initial Commit

---
+++ src/app.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Welcome to the Burngreave Works Website</h1>
+      <p>This is a React app built from scratch using Vite.</p>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/main.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './app.jsx'; // make sure the filename and casing match
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
